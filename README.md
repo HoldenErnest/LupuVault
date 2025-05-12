feb588ebf6401effe806077bb7a79810ae03b5b5
HoldenErnest <holden.ernest@icloud.com>
2025-05-12 10:03:03 -0400

    testing git

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # LupuVault
-A redefined system for LupuVault. The Web App that holds list information defined by users and has an extensive and intuitive UI.
+A redefined system for LupuVault. The Web App that holds list information defined by users. The app has an intuitive UI with extensive searching and sorting capability.
---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+import mimetypes
+mimetypes.add_type('application/javascript', '.js')
+mimetypes.add_type('application/typescript', '.ts')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SOCKET_SECRET')
+socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/draw")
+def draw():
+    return render_template("draw.html")
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
